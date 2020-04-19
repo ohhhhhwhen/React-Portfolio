430d4b751e050b3f1fbda91b219157f9d6d8a09a
Owen Chanthala <owenchanthala@gmail.com>
2020-04-19 14:27:55 -0700

    Updates

---
+++ precache-manifest.7a52bbb74aca619f46648d4c06428091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0287e1906459042cf06cfb8d1e51220",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "180d745c475fa87f64c7",
+    "url": "/React-Portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "adbb7f970e2faf654d34",
+    "url": "/React-Portfolio/static/css/main.98366425.chunk.css"
+  },
+  {
+    "revision": "180d745c475fa87f64c7",
+    "url": "/React-Portfolio/static/js/2.a8c8da45.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/React-Portfolio/static/js/2.a8c8da45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbb7f970e2faf654d34",
+    "url": "/React-Portfolio/static/js/main.76f5c397.chunk.js"
+  },
+  {
+    "revision": "910ea996016562bed44c",
+    "url": "/React-Portfolio/static/js/runtime-main.0af1ea3a.js"
+  }
+]);
